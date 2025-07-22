ec9193a5647b571cd0e323815d8f6448457a13de
Rahul Muthukrishnan <rahulm2038@gmail.com>
2025-07-22 14:37:48 +0530

    Switched to app.py and updated requirements

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Drip Mate backend is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    return jsonify({
+        "message": "Received your data!",
+        "your_input": data
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
